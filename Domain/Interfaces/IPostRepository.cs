3848d4374049482f5c04bc44b18a5d4f9c2e9590
Max <max.frolov321@gmail.com>
2022-10-04 10:48:00 +0300

    Fix using

--- Domain/Interfaces/IPostRepository.cs
+++ Domain/Interfaces/IPostRepository.cs
@@ -1,13 +1,8 @@
 ﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
-    public interface IPostRepository:IRepository<Post>
+    public interface IPostRepository : IRepository<Post>
     {
         Task<IEnumerable<Post>> GetAllWithDetailsAsync();
 
--- Domain/Interfaces/IRepository.cs
+++ Domain/Interfaces/IRepository.cs
@@ -1,13 +1,8 @@
 ﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
-    public interface IRepository<T> where T:BaseEntity
+    public interface IRepository<T> where T : BaseEntity
     {
         Task<IEnumerable<T>> GetAllAsync();
 
